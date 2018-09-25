6becbcdc13722ccff518c05e0cf3e4435d1df5fc
nurdelaa <36226843+nurdelaa@users.noreply.github.com>
2018-09-25 22:46:58 +0700

    Update latihan

--- src/main/java/com/apap/tutorial3/controller/CarController.java
+++ src/main/java/com/apap/tutorial3/controller/CarController.java
@@ -1,10 +1,12 @@
 package com.apap.tutorial3.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,16 +27,18 @@ public class CarController {
 			@RequestParam(value = "amount", required = true) Integer amount) {
 		CarModel car = new CarModel(id,brand,type,price,amount);
 		mobilService.addCar(car);
+		System.out.println("jumlah "+mobilService.getCarList().size());
+
 		return "add";	
 	}
 	
-	@RequestMapping("/car/view")
-	public String view(@RequestParam("id") String id, Model model) {
-		CarModel archive = mobilService.getCarDetail(id);
-		
-		model.addAttribute("car", archive);
-		return "view-car";
-	}
+//	@RequestMapping("/car/view")
+//	public String view(@RequestParam("id") String id, Model model) {
+//		CarModel archive = mobilService.getCarDetail(id);
+//		
+//		model.addAttribute("car", archive);
+//		return "view-car";
+//	}
 	
 	@RequestMapping("/car/viewall")
 	public String viewall(Model model) {
@@ -44,5 +48,60 @@ public class CarController {
 		return "viewall-car";
 	}
 	
+	@RequestMapping(value = {"/car/view", "/car/view/{id}"})
+	public String challengePath(@PathVariable Optional<String> id, Model model) {
+		if(id.isPresent()) {
+			System.out.println("masuk pak eko");
+
+			List<CarModel> archive = mobilService.getCarList();
+
+			int count = mobilService.getCarList().size();
+
+			while(count>1) {
+				count--;
+				System.out.println("masuk pak ");
+				
+				if(archive.get(count-1).getId().equals(id)) {
+					
+					model.addAttribute("car", archive.get(count-1));
+					return "view-car";		
+				}
+			}
+		}
+
+		return "noIdCar";
+	}
+	
+	@RequestMapping(value = {"/car/update","/car/update/{id}/amount/{amount}"})
+	public String updateAmount( @PathVariable("id") Optional<String> id,
+			@PathVariable("amount") Integer amount,Model model) {
+		if(id.isPresent()) {
+			List<CarModel> listCar = mobilService.getCarList();
+			for(int i = 0; i<listCar.size(); i++) {
+				if(listCar.get(i).getId().equals(id)) {
+					listCar.get(i).setAmount(amount);
+					model.addAttribute("car", listCar.get(i));
+					return "update-amount";		
+				}
+			}
+		}
+		return "noIdCar";
+	}
+	
+	@RequestMapping(value = {"/car/delete","/car/delete/{id}"})
+	public String delete( @PathVariable("id") Optional<String> id,Model model) {
+		if(id.isPresent()) {
+			List<CarModel> listCar = mobilService.getCarList();
+			for(int i = 0; i<listCar.size(); i++) {
+				if(listCar.get(i).getId().equals(id)) {
+					model.addAttribute("car", listCar.get(i));
+					listCar.get(i).setId(null);
+					return "deleted";		
+				}
+			}
+		}
+		return "noIdCar";
+	}
+
 
 }
